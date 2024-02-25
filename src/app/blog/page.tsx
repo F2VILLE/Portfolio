371aaadd0a829a8c5170d74b4dc0b744c1f5a107
F2Ville <feliciendeville@gmail.com>
2024-02-25 04:33:41 +0100

    Fixing little error

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,6 @@ export default function Blog() {
       <main className={styles.main}>
         <div className={styles.greetings}>
           <h1 className={styles.title}>Hi ! I'm <span className="gradient-text">F2Ville</span></h1>
-          <p>I am a {age} years old Computer Science Student at ULB, and</p>
           <p>I like creating things !</p>
         </div>
       </main>
