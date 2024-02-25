6f735bf6a62d1fc13d017aee7a0296bbece928b1
F2Ville <feliciendeville@gmail.com>
2024-02-25 04:29:58 +0100

    Starting to get Responsive

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,8 @@
-//import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.tsx'
 
-
-export default function Home() {
-  const age = Math.floor((new Date().getTime() - new Date("2004-06-03").getTime()) / 1000 / 60 / 60 / 24 / 365.25);
+export default function Blog() {
   return (
-    <>
+   <>
       <main className={styles.main}>
         <div className={styles.greetings}>
           <h1 className={styles.title}>Hi ! I'm <span className="gradient-text">F2Ville</span></h1>
@@ -13,6 +10,6 @@ export default function Home() {
           <p>I like creating things !</p>
         </div>
       </main>
-    </>
-  );
+    </> 
+  )
 }
